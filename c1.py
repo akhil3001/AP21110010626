26dfb570df841f72aa6c2351d3d3862f4cae35b0
akhil3001 <akhilesh_ramayanam@srmap.edu.in>
2023-11-01 10:19:42 +0530

    cll

---
+++ c1.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class CyclicLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            self.head.next = self.head
+        else:
+            temp = self.head
+            while temp.next != self.head:
+                temp = temp.next
+            temp.next = new_node
+            new_node.next = self.head
+
+    def display(self):
+        if not self.head:
+            return
+        temp = self.head
+        while True:
+            print(temp.data, end=' -> ')
+            temp = temp.next
+            if temp == self.head:
+                break
+        print()
+
+if __name__ == "__main__":
+    clist = CyclicLinkedList()
+    clist.append(1)
+    clist.append(2)
+    clist.append(3)
+    clist.append(4)
+
+    clist.display()
